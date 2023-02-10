2d57ef3af41af6ef17e50f5e9f2b4e470bd4ff49
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-10 13:22:13 -0500

    service added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Service working
+
+Tu run the setup.py, you must clone this repository in the `/root` folder or change the directory in line 9 of the `.service` file.
---
+++ setup.py
@@ -0,0 +1,12 @@
+# Run this if you want to have this as a service
+import os
+
+def main():
+	print("Setting the openVPN")
+	os.system("cp send_mqtt.service /etc/systemd/system")
+	os.system("systemctl enable send_mqtt.service")
+	os.system("systemctl start send_mqtt.service")
+
+	print("done")
+if __name__ == '__main__':
+	main()
